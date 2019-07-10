10f18332bcdbbb826921ce3fa01f9bcd24032416
Chris Ford <chrisford51@gmail.com>
2019-07-10 19:09:00 -0400

    Added solutions to compare, other minor changes

---
+++ Exercises/Multiply-Divide/function.js
@@ -0,0 +1,33 @@
+// function multiplyBy()
+// {
+//   num1 = document.getElementByID("firstNumber").value;
+//   num2 = document.getElementByID("secondNumber").value;
+//   document.getElementByID("result").innerHTML = num1 * num2;
+// }
+//
+// function divideBy()
+// {
+//   num1 = document.getElementById("firstNumber").value;
+//   num2 = document.getElementById("secondNumber").value;
+//   document.getElementById("result").innerHTML = num1 / num2;
+// }
+
+
+
+
+// ### Correct Solution ###
+
+
+function multiplyBy()
+{
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+        document.getElementById("result").innerHTML = num1 * num2;
+}
+
+function divideBy()
+{
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+document.getElementById("result").innerHTML = num1 / num2;
+}
